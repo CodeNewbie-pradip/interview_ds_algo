820f4b7e66d88977595e96ed3dc68982ae6457d6
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-08-15 16:40:10 +0530

    Leetcode easy

---
+++ Arrays/Leetcode Easy/Lemonade Change.cpp
@@ -0,0 +1,28 @@
+/*
+Leetcode difficulty         : Easy
+Leetcode Related Topic      : Array, Greedy
+*/
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fiveDollar = 0;
+        int tenDollar = 0;
+        
+        for(auto bill : bills) {
+            if(bill == 5) {
+                fiveDollar++;
+            } else if(bill == 10 && fiveDollar > 0) {
+                fiveDollar--;
+                tenDollar++;
+            } else if(tenDollar > 0 && fiveDollar > 0) { // when returning 15 dollars
+                tenDollar--; 
+                fiveDollar--;
+            } else if(fiveDollar >= 3) {
+                fiveDollar -= 3;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
