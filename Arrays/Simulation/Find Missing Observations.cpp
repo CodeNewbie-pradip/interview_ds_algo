3af4a788f17fc56f948368e52abcbe891673ace6
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-09-06 08:38:26 +0530

    Leetcode 2028

--- Arrays/Simulation/Find Missing Observations.cpp
+++ Arrays/Simulation/Find Missing Observations.cpp
@@ -1,12 +1,12 @@
 /*
-Leetcode Link               : Greedy Distribution
+Leetcode Link               : https://leetcode.com/problems/find-missing-observations/
 Leetcode difficulty         : Medium
 Leetcode Related Topic      : Arrays    Math    Simulation
 */
 
 //------------------------------Solution - in - C++ -----------------------------------------------------------
 
-//Approach 1: https://leetcode.com/problems/find-missing-observations/
+//Approach 1: Greedy Distribution
 //T.C       : O(m+n)
 //S.C       : O(n)
 //-------------------------------------------------------------------------------------------------------------
@@ -34,4 +34,31 @@ public:
     }
 };
 
+//---------------------------------------------------------------------------------------------------------------
+//Approach 2: More Controlled Distribution
+//T.C       : O(m+n)
+//S.C       : O(n)
+//-------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m=rolls.size(); //size of the rolls
+        int totalSum=mean*(n+m); //total sum of the rolls can be calculated
+        int sumOfRolls=accumulate(rolls.begin(), rolls.end(), 0);
+        int missingSum=totalSum-sumOfRolls;
+        
+        //check if sum is valid or not
+        if(missingSum<n ||missingSum>n*6){
+            return {};
+        }
+        vector<int>result(n, missingSum/n);
+        int remainder=missingSum%n;
+        for(int i=0; i<n && remainder>0; i++){
+            result[i]++;
+            remainder--;
+        }
+        return result;
+    }
+};
 //---------------------------------------------------------------------------------------------------------------
